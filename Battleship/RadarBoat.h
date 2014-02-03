ca61eea3f43eca10399557465e18299092ef1142
robertschneidman <robert.schneidman@gmail.com>
2014-02-03 15:49:38 -0500

    radar boat added

---
+++ Battleship/RadarBoat.h
@@ -0,0 +1,13 @@
+//
+//  RadarBoat.h
+//  Battleship
+//
+//  Created by Robert Schneidman on 2/3/2014.
+//  Copyright (c) 2014 COMP-361. All rights reserved.
+//
+
+#import "Ship.h"
+
+@interface RadarBoat : Ship
+
+@end
---
+++ Battleship/RadarBoat.m
@@ -0,0 +1,13 @@
+//
+//  RadarBoat.m
+//  Battleship
+//
+//  Created by Robert Schneidman on 2/3/2014.
+//  Copyright (c) 2014 COMP-361. All rights reserved.
+//
+
+#import "RadarBoat.h"
+
+@implementation RadarBoat
+
+@end
