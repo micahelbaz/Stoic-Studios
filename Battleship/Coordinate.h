d35bf72ce210bfa5fcf5f674eed97b16d0007b83
robertschneidman <robert.schneidman@gmail.com>
2014-02-05 15:02:53 -0500

    added coordinate initializer

--- Battleship/Coordinate.h
+++ Battleship/Coordinate.h
@@ -2,24 +2,19 @@
 //  Coordinate.h
 //  Battleship
 //
-//  Created by Robert Schneidman on 1/27/2014.
+//  Created by Robert Schneidman on 2/5/2014.
 //  Copyright (c) 2014 COMP-361. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
+#import "DirectionType.h"
 @interface Coordinate : NSObject
-
-typedef enum directionType
-{
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST
-} Direction;
 
 @property int xCoord;
 @property int yCoord;
 @property Direction direction;
 
+- (instancetype) initWithXCoordinate: (int)xCoordinate
+                         YCoordinate:(int) yCoordinate
+                          initiallyFacing:(Direction) facingDirection;
 @end
---
+++ Battleship/Map.h
@@ -0,0 +1,15 @@
+//
+//  Map.h
+//  Battleship
+//
+//  Created by Robert Schneidman on 2/5/2014.
+//  Copyright (c) 2014 COMP-361. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Map : NSObject
+
+@property (strong, nonatomic) NSArray* grid;
+
+@end
